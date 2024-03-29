789ebbf8536300a2ca8267e7f0c6c98b9a0718d1
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-28 20:00:44 -0700

    OrderService Microservice - Configure RabbitMQ Step By Step

---
+++ order-service/src/main/java/com/codefusionhub/orderservice/config/RabbitMQConfig.java
@@ -0,0 +1,64 @@
+package com.codefusionhub.orderservice.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.order.name}")
+    private String orderQueue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String orderExchange;
+
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String routingKey;
+
+
+
+
+    //spring bean for queue - order queue
+    @Bean
+    public Queue orderQueue(){
+        return new Queue(orderQueue);
+    }
+
+    //spring bean for exchange
+    @Bean
+    public TopicExchange exhange(){
+        return new TopicExchange(orderExchange);
+    }
+
+    //spring bean for binding between exchange and queue using routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(orderQueue())
+                .to(exhange())
+                .with(routingKey);
+    }
+
+    //message
+    @Bean
+    public MessageConverter converter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //configure rabbit-template
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+
+        return rabbitTemplate;
+    }
+
+}
